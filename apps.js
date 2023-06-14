e6da0ae5dedb3726fb1ef14c669dda54cafd4d96
Lamyae Chafik <lamyaechafik@Lamyaes-MBP.lan>
2023-06-14 11:25:47 -0400

    message

---
+++ apps.js
@@ -0,0 +1,11 @@
+//use array inside of object
+
+const adventure = {
+    name: "lamyae",
+    hitPoint: "10",
+    belongings: ["staff", "portion", "medicine" ]
+}
+
+//access the values in the array
+
+console.log(adventure.belongings);
