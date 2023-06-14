b49513187cf7d4e8e23d823963ce61d897d97dd7
Lamyae Chafik <lamyaechafik@Lamyaes-MBP.lan>
2023-06-14 11:56:09 -0400

    message

--- apps.js
+++ apps.js
@@ -1,20 +1,20 @@
 //use array inside of object
 
-const adventure = {
-    name: "lamyae",
-    hitPoint: "10",
-    belongings: ["staff", "portion", "medicine" ],
-    companion: {
-        name: "stinky",
-        type: "cat"
-        belongings: ["ball of yarn", "healing portion"]
-    }
-}
+// const adventure = {
+//     name: "lamyae",
+//     hitPoint: "10",
+//     belongings: ["staff", "portion", "medicine" ],
+//     companion: {
+//         name: "stinky",
+//         type: "cat"
+//         belongings: ["ball of yarn", "healing portion"]
+//     }
+// }
 
 // //access the values in the array
 
-console.log(`my name companion is ${adventure.belongings.name}`);
-console.log(`one of my companiong belongings os ${adventure.companion.belongings[2]}`);const movies = [ { title: "Tokyo Story" },  { title: "Paul Blart: Mall Cop" }, { title: "L'Avventura" } ];
+// console.log(`my name companion is ${adventure.belongings.name}`);
+// console.log(`one of my companiong belongings os ${adventure.companion.belongings[2]}`);const movies = [ { title: "Tokyo Story" },  { title: "Paul Blart: Mall Cop" }, { title: "L'Avventura" } ];
 
 
 // console.log(' first belonging: ${adventure.belonging[0]}')
@@ -25,12 +25,38 @@ console.log(`one of my companiong belongings os ${adventure.companion.belongings
 //     console.log(adventure.belongings[i])
 // }
 
-//ONJ within OBJ
-const movies = [ { title: "Tokyo Story" }, 
- { title: "Paul Blart: Mall Cop" }, 
- { title: "L'Avventura" } ];
+// //ONJ within OBJ
+// const movies = [ { title: "Tokyo Story" }, 
+//  { title: "Paul Blart: Mall Cop" }, 
+//  { title: "L'Avventura" } ];
 
-console.log(movies[0].title);
-for (let i=0; i < movies.length; i++) {
-	console.log(movies[i].title);
-}
+// console.log(movies[0].title);
+// for (let i=0; i < movies.length; i++) {
+// 	console.log(movies[i].title);
+// }
+
+
+
+// const foo = {
+//     arr: [1 ,2, 4]
+//     obj: {
+//         prop:'object proprety'
+//     },
+// dosomthing: () => //or we can replace it with the word function// 
+//    { 
+//     console.log("i did somthing")
+//     console.log
+// }
+// }
+// console.log(foor.arr[0]);//1
+// console.log(foo.arr)
+
+
+const foo = [
+    [1, ,2 ,3],
+    ["4", "5", "6"],
+    [7, 8, 9],
+    () => {
+        console.log("i am a finction inside an array")
+    }
+]
